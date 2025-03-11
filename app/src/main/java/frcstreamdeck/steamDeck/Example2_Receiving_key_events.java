b6de52e0ea0920d1d1643b2717a2801882ded99e
ViljamiBaker <viljamiolder@outlook.com>
2025-03-10 20:58:34 -0700

    intial finding out

---
+++ app/src/main/java/frcstreamdeck/steamDeck/Example2_Receiving_key_events.java
@@ -0,0 +1,45 @@
+package steamDeck;
+
+import de.rcblum.stream.deck.device.IStreamDeck;
+import de.rcblum.stream.deck.device.StreamDeckDevices;
+import de.rcblum.stream.deck.event.KeyEvent;
+import de.rcblum.stream.deck.event.StreamKeyListener;
+
+import java.io.IOException;
+
+public class Example2_Receiving_key_events {
+	public static void main(String[] args) throws IOException {
+		// Get the first connected (or software) ESD:
+		IStreamDeck streamDeck = StreamDeckDevices.getStreamDeck();
+		// Reset the ESD so we can display our icon on it:
+		System.out.println(streamDeck.addKeyListener(new ExampleListener()));
+		streamDeck.setBrightness(200);
+		try {
+			Thread.sleep(100_000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			Thread.currentThread().interrupt();
+		}
+	}
+	
+	public static class ExampleListener implements StreamKeyListener {
+		public void onKeyEvent(KeyEvent event) {
+			switch(event.getType()) {
+			case OFF_DISPLAY :
+				System.out.println(event.getKeyId() + ": taken off display");
+				break;
+			case ON_DISPLAY:
+				System.out.println(event.getKeyId() + ": put on display");
+				break;
+			case PRESSED:
+				System.out.println(event.getKeyId() + ": pressed");
+				break;
+			case RELEASED_CLICKED:
+				System.out.println(event.getKeyId() + ": released/clicked");
+				break;
+			default:
+				break;
+			}
+		}
+	}
+}
